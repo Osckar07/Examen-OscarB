423c0ebc8d1046880af13bbb13323fa9a792a83c
Osckar07 <50966035+Osckar07@users.noreply.github.com>
2019-06-10 13:50:03 -0600

    Final

--- Planilla/Planilla/MainWindow.xaml.cs
+++ Planilla/Planilla/MainWindow.xaml.cs
@@ -39,12 +39,7 @@ namespace Planilla
         {
             string nombre = Nombre del Empleado;
             string id = Identidad;
-            decimal dedu = (Convert.ToDecimal(horasTrabajadas.Text) * Convert.ToDecimal(pagoPorHora.Text));
-            decimal ex = (Convert.ToDecimal(horasExtras.Text) * Convert.ToDecimal(pagoHorasExtras.Text));
-            decimal IH = (((Convert.ToDecimal(horasTrabajadas.Text) * Convert.ToDecimal(pagoPorHora.Text)) * Convert.ToDecimal(0.04)));
-            decimal RA = (((Convert.ToDecimal(horasTrabajadas.Text) * Convert.ToDecimal(pagoPorHora.Text)) * Convert.ToDecimal(0.035)));
-            decimal PT = ((dedu + ex) - (IH + RA));
-            MessageBox.Show("Nombre: " + nombre + "\nIdentidad: " + id + "\nSueldo sin deducciones: " + dedu + "\nSueldo Horas Extras: " + ex + "\nDeducciones IHSS: " + IH + "\nDeducciones RAP: " + RA + "\nPago Total: " + PT);
-        }
+      
+           
     }
 }
