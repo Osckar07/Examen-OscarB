f65c5ed5e66e248b32d9e97d9951835f2c0ac588
Osckar07 <50966035+Osckar07@users.noreply.github.com>
2019-06-10 13:49:01 -0600

    Final

---
+++ Planilla/Planilla/ClassEmpleado.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Agregando el namespace necesario
+using System.ComponentModel;
+
+namespace Planilla
+{
+    public class planilla : INotifyPropertyChanged
+    {
+        // Variables miembro
+        private string nombre_del_Empleado;
+        private string identidad;
+        private int horasTrabajadas;
+        private float pagoHoraNormalTrabajo;
+        private int horasExtrasTrabajadas;
+        private float pagoHoraTrabajoExtra;
+
+        public planilla()
+        {
+            Nombre_del_Empleado = "Oscar Benites";
+            Identidad = "1007-1998-00788";
+            HorasTrabajadas = 8;
+            PagoHoraNormalTrabajo = 32000;
+            HorasExtrasTrabajadas = 5;
+            PagoHoraTrabajoExtra = 1200;
+        }
+
+        public string Nombre_del_Empleado { get => nombre_del_Empleado; set => nombre_del_Empleado = value; }
+        public string Identidad { get => identidad; set => identidad = value; }
+        public int HorasTrabajadas { get => horasTrabajadas; set => horasTrabajadas = value; }
+        public float PagoHoraNormalTrabajo { get => pagoHoraNormalTrabajo; set => pagoHoraNormalTrabajo = value; }
+        public int HorasExtrasTrabajadas { get => horasExtrasTrabajadas; set => horasExtrasTrabajadas = value; }
+        public float PagoHoraTrabajoExtra { get => pagoHoraTrabajoExtra; set => pagoHoraTrabajoExtra = value; }
+    }
+
---
+++ Planilla/Planilla/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Planilla
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string nombre = Nombre del Empleado;
+            string id = Identidad;
+            decimal dedu = (Convert.ToDecimal(horasTrabajadas.Text) * Convert.ToDecimal(pagoPorHora.Text));
+            decimal ex = (Convert.ToDecimal(horasExtras.Text) * Convert.ToDecimal(pagoHorasExtras.Text));
+            decimal IH = (((Convert.ToDecimal(horasTrabajadas.Text) * Convert.ToDecimal(pagoPorHora.Text)) * Convert.ToDecimal(0.04)));
+            decimal RA = (((Convert.ToDecimal(horasTrabajadas.Text) * Convert.ToDecimal(pagoPorHora.Text)) * Convert.ToDecimal(0.035)));
+            decimal PT = ((dedu + ex) - (IH + RA));
+            MessageBox.Show("Nombre: " + nombre + "\nIdentidad: " + id + "\nSueldo sin deducciones: " + dedu + "\nSueldo Horas Extras: " + ex + "\nDeducciones IHSS: " + IH + "\nDeducciones RAP: " + RA + "\nPago Total: " + PT);
+        }
+    }
+}
